8590161f319924c8a9eeccd836a74e16d9b4af2e
Jarbas Peixoto Júnior <jarbas.junior@gmail.com>
2020-11-25 15:59:26 -0400

    Script que auxilia a liberação de memória swap

---
+++ swap-off.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# https://help.ubuntu.com/community/SwapFaq
+export PATH="/usr/sbin:/usr/bin:/sbin:/bin"
+
+echo "========"
+echo "Before - `date`"
+echo "========"
+free -h
+
+
+sync
+
+echo 3 > /proc/sys/vm/drop_caches
+
+mem=$(LC_ALL=C free  | awk '/Mem:/ {print $4}')
+swap=$(LC_ALL=C free | awk '/Swap:/ {print $3}')
+
+if [ $mem -lt $swap ]; then
+        echo "ERROR: [$swap > $mem]not enough RAM to write swap back, nothing done" >&2
+        exit 1
+fi
+
+
+swapoff -a
+
+swapon -a
+
+echo "========"
+echo "After - `date` "
+echo "========"
+free -h
